501019a68ec166bc69e5ecd92565bf500463e6e1
harry7lee <perfect7lee@163.com>
2018-10-08 17:32:29 +0800

    Lesson 20

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,52 @@
 import React, { Component } from "react";
 import Ninjas from "./Ninjas";
+import AddNinja from "./AddNinja";
 
 class App extends Component {
   state = {
     ninjas: [
       { name: "Ryu", age: "30", belt: "black", id: 1 },
-      { name: "Ryu", age: "30", belt: "black", id: 2 },
-      { name: "Ryu", age: "30", belt: "black", id: 3 },
+      { name: "Ryu", age: "1", belt: "black", id: 2 },
+      { name: "Ryu", age: "1", belt: "black", id: 3 },
       { name: "Ryu", age: "30", belt: "black", id: 4 }
     ]
   };
+  addNinja = ninja => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({ ninjas: ninjas });
+  };
+  deleteNinja = id => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id;
+    });
+    this.setState({ ninjas: ninjas });
+  };
+  // handleClick = e => {
+  //   console.log(e.target);
+  //   this.setState({
+  //     name: "Yoshi"
+  //   });
+  //   console.log(this.state);
+  // };
+  // handleMouseOver(e) {
+  //   console.log(e.target, e.pageX);
+  // }
+  // handleCopy(e) {
+  //   console.log(e.target);
+  // }
   render() {
     return (
       <div className="App">
         <h1>My first React App!</h1>
-        <Ninjas ninjas={this.state.ninjas} />
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja} />
+        <AddNinja addNinja={this.addNinja} />
+        {/* <p>
+          My name is : {this.state.name} and I am {this.state.age}
+        </p>
+        <button onClick={this.handleClick}>click me</button>
+        <button onMouseOver={this.handleMouseOver}>Hover me</button>
+        <p onCopy={this.handleCopy}>What we think, we become</p> */}
       </div>
     );
   }
--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,19 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Ninjas extends Component {
-  render() {
-    const { ninjas } = this.props;
-    const ninjaList = ninjas.map(ninja => {
+const Ninjas = ({ ninjas, deleteNinja }) => {
+  const ninjaList = ninjas.map(ninja => {
+    if (ninja.age > 20) {
       return (
         <div className="ninja" key={ninja.id}>
           <div>Name: {ninja.name}</div>
           <div>age: {ninja.age}</div>
           <div>belt: {ninja.belt}</div>
+          <button
+            onClick={() => {
+              deleteNinja(ninja.id);
+            }}
+          >
+            Delete Ninja
+          </button>
         </div>
       );
-    });
-    return <div class="ninja-list">{ninjaList}</div>;
-  }
-}
+    } else {
+      return null;
+    }
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
+// class Ninjas extends Component {
+//   render() {
+//     const { ninjas } = this.props;
+//     const ninjaList = ninjas.map(ninja => {
+//       return (
+//         <div className="ninja" key={ninja.id}>
+//           <div>Name: {ninja.name}</div>
+//           <div>age: {ninja.age}</div>
+//           <div>belt: {ninja.belt}</div>
+//         </div>
+//       );
+//     });
+//     return <div className="ninja-list">{ninjaList}</div>;
+//   }
+// }
 
 export default Ninjas;
